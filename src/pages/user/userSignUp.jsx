ea24ade4313532fe8224c8432b5118976f90be55
Suresh Timma <148480722+SureshTimma@users.noreply.github.com>
2025-05-12 12:31:42 +0530

    Update userSignUp.jsx

--- src/pages/user/userSignUp.jsx
+++ src/pages/user/userSignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import config from "../../config";
+import config from "/src/config";
 const UserSignup = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
