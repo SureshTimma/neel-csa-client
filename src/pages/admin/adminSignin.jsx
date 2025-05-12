3b9019d0eeeba7194ea3e2f11a897485d5cab4ea
Suresh Timma <148480722+SureshTimma@users.noreply.github.com>
2025-05-12 12:30:35 +0530

    Update adminSignin.jsx

--- src/pages/admin/adminSignin.jsx
+++ src/pages/admin/adminSignin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import config from "../../config";
+import config from "/src/config";
 
 const AdminSignin = () => {
   const navigate = useNavigate();
